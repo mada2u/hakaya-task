e101e06471294ac3d3d406c1fcfb6f664d829813
Mahmoud Ebada <>
2019-02-18 06:00:24 +0200

    Feat fillable and spatialFields arrays
    - set fillable and spatialFields arrays
    - conform SpatialTrait

--- app/Contact.php
+++ app/Contact.php
@@ -2,9 +2,21 @@
 
 namespace App;
 
+use Grimzy\LaravelMysqlSpatial\Eloquent\SpatialTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    //
+    use SpatialTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'email', 'phone', 'comments'];
+
+    protected $spatialFields = [
+        'location'
+    ];
 }
