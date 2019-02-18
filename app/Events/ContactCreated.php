ea106558f0b9623d3087b93dc6bc88b4c8403600
Mahmoud Ebada <>
2019-02-18 07:11:09 +0200

    Feat configure event
    pass contact object to use it on Listener

--- app/Events/ContactCreated.php
+++ app/Events/ContactCreated.php
@@ -2,26 +2,28 @@
 
 namespace App\Events;
 
+use App\Contact;
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
 class ContactCreated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $contact;
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Contact $contact)
     {
-        //
+        $this->contact = $contact;
     }
 
     /**
