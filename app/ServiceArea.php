7797a4fe8241bf8a021618f92e940417a502c084
Mahmoud Ebada <>
2019-02-18 05:11:19 +0200

    Add fillable and spatialFields arrays
    - Set fillable and spatialFields arrays
    - conform  SpatialTrait

--- app/ServiceArea.php
+++ app/ServiceArea.php
@@ -2,9 +2,23 @@
 
 namespace App;
 
+use Grimzy\LaravelMysqlSpatial\Eloquent\SpatialTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class ServiceArea extends Model
 {
-    //
+	use SpatialTrait;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'area'];
+
+    public $timestamps = false;
+
+    protected $spatialFields = [
+        'area'
+    ];
 }
