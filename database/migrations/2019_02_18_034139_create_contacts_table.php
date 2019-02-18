89891aad3c9c9c422bcbe9ce1d4c1e58f0ffdbe3
Mahmoud Ebada <>
2019-02-18 05:43:08 +0200

    Add contact table migration
    - $ php artisan make:migration create_contacts_table
    - $ php artisan migrate

---
+++ database/migrations/2019_02_18_034139_create_contacts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',64);
+            $table->string('email',32);
+            $table->string('phone',16);
+            $table->text('comments');
+            $table->point('location');
+            $table->tinyInteger('in_area')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
