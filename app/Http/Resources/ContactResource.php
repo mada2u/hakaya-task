f625ad51eb5001b13020ea21ba5720a544230c0e
Mahmoud Ebada <>
2019-02-18 06:14:58 +0200

    Add ContactResource
    $ php artisan make:resource ContactResource

---
+++ app/Http/Resources/ContactResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ContactResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'comments' => $this->comments,
+            'location' => [
+                'lat' => $this->location->getLat(),
+                'lng' => $this->location->getLng()
+            ]
+        ];
+    }
+}
